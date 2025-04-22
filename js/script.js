09fe433845b07e80f0ac43755c142c1d39d1bd0c
Челюбеева Дарья Геннадьевна <design2@imperia.local>
2025-04-22 14:19:28 +0300

    animation burger

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-let headerBurger = document.querySelector('.header__burger');
+let burgerOpen = document.querySelector('.header__burger');
+let burgerClose = document.querySelector('.header__burger-close');
 let headerMenu = document.querySelector('.header__menu-burger');
 
-headerBurger.addEventListener('click', function() {
-    headerMenu.classList.toggle('visually-hidden');
-    
-    if (!headerMenu.classList.contains('visually-hidden')) {
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.overflow = 'auto';
-    }
+burgerOpen.addEventListener('click', function() {
+    headerMenu.classList.remove('visually-hidden');
+    burgerClose.classList.add('open');
+    burgerOpen.classList.add('visually-hidden');
+
+    document.body.style.overflow = 'hidden';   
 });
 
-  
+  burgerClose.addEventListener('click', function() {
+    headerMenu.classList.add('visually-hidden');
+    burgerClose.classList.remove('open');
+    burgerOpen.classList.remove('visually-hidden');
+
+    document.body.style.overflow = 'auto';
+  })
