c354f5f900bb0e60f69c9aa44c43ad01a55e0300
Челюбеева Дарья Геннадьевна <design2@imperia.local>
2025-04-22 16:38:56 +0300

    fix script

--- js/script.js
+++ js/script.js
@@ -12,10 +12,6 @@ checkScreenSize();
 
 let burgerOpen = document.querySelector('.header__burger');
 let burgerClose = document.querySelector('.header__burger-close');
-// let headerMenu = document.querySelector('.header__menu-burger');
-
-
-
 burgerOpen.addEventListener('click', function() {
     headerMenu.classList.remove('visually-hidden');
     burgerClose.classList.add('open');
